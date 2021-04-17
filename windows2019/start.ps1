89c3161281ab95ebb15fafa200a09bda1d693ca6
Pablo Monteiro <pablohenriquem@hotmail.com>
2021-04-16 22:31:55 -0300

    Adicionando instalações no windows

--- windows2019/start.ps1
+++ windows2019/start.ps1
@@ -19,30 +19,28 @@ if ((Test-Path Env:AZP_WORK) -and -not (Test-Path $Env:AZP_WORK)) {
   New-Item $Env:AZP_WORK -ItemType directory | Out-Null
 }
 
-New-Item "\azp\agent" -ItemType directory | Out-Null
-
 # Let the agent ignore the token env variables
 $Env:VSO_AGENT_IGNORE = "AZP_TOKEN,AZP_TOKEN_FILE"
 
 Set-Location agent
 
-Write-Host "0. Download and Install Python 3.8.0" -ForegroundColor Cyan
+Write-Host "1. Install .Net Core" -ForegroundColor Cyan
 
-$wc1 = New-Object System.Net.WebClient
-$wc1.DownloadFile("https://www.python.org/ftp/python/3.8.0/python-3.8.0-amd64.exe", "$(Get-Location)\python-3.8.0.exe")
-$Command = "$(Get-Location)\python-3.8.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0 TargetDir=C:\azp\agent\_work\_tool\Python\3.8.0\x64"
-Invoke-Expression $Command
-#New-Item -Path "C:\azp\agent\_work\_tool\Python\3.8.0" -Name "x64.complete" -ItemType "file"
+$wc2 = New-Object System.Net.WebClient
+$wc2.DownloadFile("https://dot.net/v1/dotnet-install.ps1", "$(Get-Location)\dotnet-install.ps1")
+$Command2 = "./dotnet-install.ps1 -Channel LTS"
+Invoke-Expression $Command2
 
-Write-Host "1. Determining matching Azure Pipelines agent..." -ForegroundColor Cyan
+Write-Host "2. Determining matching Azure Pipelines agent..." -ForegroundColor Cyan
 
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$(Get-Content ${Env:AZP_TOKEN_FILE})"))
 $package = Invoke-RestMethod -Headers @{Authorization=("Basic $base64AuthInfo")} "$(${Env:AZP_URL})/_apis/distributedtask/packages/agent?platform=win-x64&`$top=1"
 $packageUrl = $package[0].Value.downloadUrl
 
 Write-Host $packageUrl
+Write-Host $(Get-Location)
 
-Write-Host "2. Downloading and installing Azure Pipelines agent..." -ForegroundColor Cyan
+Write-Host "3. Downloading and installing Azure Pipelines agent..." -ForegroundColor Cyan
 
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($packageUrl, "$(Get-Location)\agent.zip")
@@ -51,7 +49,7 @@ Expand-Archive -Path "agent.zip" -DestinationPath "\azp\agent"
 
 try
 {
-  Write-Host "3. Configuring Azure Pipelines agent..." -ForegroundColor Cyan
+  Write-Host "4. Configuring Azure Pipelines agent..." -ForegroundColor Cyan
 
   .\config.cmd --unattended `
     --agent "$(if (Test-Path Env:AZP_AGENT_NAME) { ${Env:AZP_AGENT_NAME} } else { ${Env:computername} })" `
@@ -61,11 +59,8 @@ try
     --pool "$(if (Test-Path Env:AZP_POOL) { ${Env:AZP_POOL} } else { 'Default' })" `
     --work "$(if (Test-Path Env:AZP_WORK) { ${Env:AZP_WORK} } else { '_work' })" `
     --replace
-
-  Write-Host "3.1. Configuring Python Cache..." -ForegroundColor Cyan
-  New-Item -Path "C:\azp\agent\_work\_tool\Python\3.8.0" -Name "x64.complete" -ItemType "file"
   
-  Write-Host "4. Running Azure Pipelines agent..." -ForegroundColor Cyan
+  Write-Host "5. Running Azure Pipelines agent..." -ForegroundColor Cyan
   .\run.cmd
 }
 finally
